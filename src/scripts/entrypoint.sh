be692d08e3dcfdd49ff65b3bbd84c77fe9b29859
Vladimir Jelovac <vladimir@jelovac.net>
2018-11-07 21:48:02 +0100

    Fixing

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -15,19 +15,26 @@ fi
 EXTRA_OPTIONS=( "$@" )
 
 EXECUTABLE="/opt/hlds/hlds_run"
-GAME="${GAME:-cstrike}"
 MAXPLAYERS="${MAXPLAYERS:-32}"
 START_MAP="${START_MAP:-de_dust2}"
 SERVER_NAME="${SERVER_NAME:-Counter-Strike 1.6 Server}"
 
-OPTIONS=( "-game" "${GAME}" "+maxplayers" "${MAXPLAYERS}" "+map" "${START_MAP}" "+hostname" "\"${SERVER_NAME}\"")
+OPTIONS=( "-game" "cstrike" "+maxplayers" "${MAXPLAYERS}" "+map" "${START_MAP}" "+hostname" "\"${SERVER_NAME}\"")
 
 if [ -z "${RESTART_ON_FAIL}" ]; then
     OPTIONS+=('-norestart')
 fi
 
-if [ -n "${ADMIN_STEAM}" ]; then
-    echo "\"STEAM_${ADMIN_STEAM}\" \"\"  \"abcdefghijklmnopqrstu\" \"ce\"" >> "/opt/hlds/cstrike/addons/amxmodx/configs/users.ini"
+if [ -n "${SERVER_ADMIN_STEAM_ID}" ]; then
+
+    set +e
+    adminAlreadyAdded=$(cat "/opt/hlds/cstrike/addons/amxmodx/configs/users.ini" | grep -c "${SERVER_ADMIN_STEAM_ID}")
+    set -e
+
+    if [ $adminAlreadyAdded -eq 0 ]; then
+        echo "\"STEAM_${SERVER_ADMIN_STEAM_ID}\" \"\"  \"abcdefghijklmnopqrstu\" \"ce\"" > "/opt/hlds/cstrike/addons/amxmodx/configs/users.ini"
+    fi
+
 fi
 
 set > "${CONFIG_FILE}"
